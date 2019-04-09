6221d37900894078cbf0cfeaedd85e0dce2bf626
github username <bntinnin7@gmail.com>
2019-04-09 17:43:35 -0400

    add input bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import SearchBar from './components/SearchBar'
 
-const App = () => {
-    return <div>App goes here</div>
+class App extends React.Component {
+
+    render() {
+
+        return (
+        <div>
+            <SearchBar />
+        </div>
+        )
+    }
 }
 
 export default App
