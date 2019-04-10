7ec7894b881b2835bd0927451cb98283504db6b1
github username <bntinnin7@gmail.com>
2019-04-09 21:32:56 -0400

    add .env to gitignore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
 
+
 class App extends React.Component {
+    //setup axios request to api based on search term
+   
 
     render() {
         return (
-        <div className="ui container">
+        <div className="ui container" style={{ marginTop: '10px' }}>
             <SearchBar />
         </div>
         )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,14 +14,12 @@ class SearchBar extends Component {
         //TODO: make a call to callback from parent component
     }
 
-
     render() {
         return (
         <div className="ui segment">
             <form className="ui form" onSubmit={this.onEnter}>
                 <input type="text" placeholder="Search GIPHY..." value={this.state.term} onChange={this.onInputChange} />
             </form>
-            {console.log(this.state.term)}
         </div>
         )
     }
